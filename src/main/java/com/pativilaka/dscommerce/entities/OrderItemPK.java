33694e65977060aed2a6665fdaf736e11620a9d3
pativilaka <p.vilaka@gmail.com>
2023-07-17 10:35:48 -0300

    Add import.sql

--- src/main/java/com/pativilaka/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/pativilaka/dscommerce/entities/OrderItemPK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
 
@@ -31,5 +33,23 @@ public class OrderItemPK {
 
     public void setOrder(Order order) {
         this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+
+        if (!Objects.equals(product, that.product)) return false;
+        return Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = product != null ? product.hashCode() : 0;
+        result = 31 * result + (order != null ? order.hashCode() : 0);
+        return result;
     }
 }
